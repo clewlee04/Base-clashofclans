49a1cea4e5eb39b0466a20b465a9ea4809672219
clewlee04 <clewlee04@gmail.com>
2026-09-22 16:10:34 +0700

    config.js

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
-// Isi dari Supabase Dashboard > Project Settings > API.
-// Gunakan Publishable key, BUKAN service_role/secret key.
 const SUPABASE_URL = "https://pjvlxkjtguuqqykacopx.supabase.co";
 const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_4eZBQoXtUL8fUkYcLCXpCQ_5shBhtLB";
-const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
+
+window.supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY
+);
